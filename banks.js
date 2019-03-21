0a1da65b105bb7e67ff3d7226d80cf21b979ae57
Danh Luong <danhluong@kms-technology.com>
2019-03-21 11:08:25 +0700

    combine all metadata to banks.js

---
+++ banks.js
@@ -0,0 +1,356 @@
+exports.banks = {
+    1111123456789: {
+        name: "MedEdSample",
+        taxonomies: {
+            "MedEd Test Sample": [
+                "Sample",
+                "AMEE"
+            ]
+        }
+    },
+    1111222222222: {
+        name: "UKBasicScience",
+        taxonomies: {
+            "UK Basic Sciences - Systems": [
+                "Cardiovascular system",
+                "Endocrine system",
+                "Gastrointestinal system",
+                "Haematological system",
+                "Immune system",
+                "Musculoskeletal system",
+                "Nervous system",
+                "Psychological system",
+                "Renal/Urinary system",
+                "Reproductive system",
+                "Respiratory system",
+                "Skin/Cancer tissue"
+            ],
+            "UK Basic Sciences - Disciplines": [
+                "Anatomy",
+                "Biochemistry",
+                "Cell biology",
+                "Embryology",
+                "Energy metabolism",
+                "Epidemiology",
+                "Genetics",
+                "Histology",
+                "Immunology",
+                "Microbiology",
+                "Molecular biology",
+                "Neurology",
+                "Nutrition",
+                "Pathology",
+                "Pathophysiology",
+                "Pharmacology/Therapeutics",
+                "Physiology",
+                "Public health"
+            ]
+        }
+    },
+    1111333333333: {
+        name: "UKClinicalMedicine",
+        taxonomies: {
+            "UK Clinical Medicine - Competencies": [
+                "Communication Skills",
+                "Ethics",
+                "Interpretation of Results",
+                "Investigation",
+                "Making a Diagnosis/decision",
+                "Management",
+                "Mechanisms of Disease",
+                "Peri-Operative Care",
+                "Prevention",
+                "Prognosis",
+                "Symptoms and Signs"
+            ],
+            "UK Clinical Medicine - Specialties": [
+                "Anaesthetics",
+                "Dermatology",
+                "Emergency Medicine",
+                "Endocrinology",
+                "ENT",
+                "Gastroenterology",
+                "General Medicine",
+                "Haematology",
+                "Infectious Diseases",
+                "Molecular Biology/Genetics",
+                "Obstetrics and Gynaecology",
+                "Ophthalmology",
+                "Orthopaedics",
+                "Paediatrics",
+                "Pathology/Immunology",
+                "Psychiatry",
+                "Rheumatology",
+                "Surgery"
+            ],
+            "UK Clinical Medicine - Systems": [
+                "Cardiovascular system",
+                "Endocrine system",
+                "Gastrointestinal system",
+                "Haematological system",
+                "Immune system",
+                "Musculoskeletal system",
+                "Nervous system",
+                "Psychological system",
+                "Renal/Urinary system",
+                "Reproductive system",
+                "Respiratory system",
+                "Skin/Cancer tissue"
+            ]
+        }
+    },
+    1111444444441: {
+        name: "USMLEStep1",
+        taxonomies: {
+            "Organ Systems": [
+                "Cardiovascular system",
+                "Endocrine and Metabolic disorders system",
+                "Gastrointestinal system",
+                "General system",
+                "Hematopoietic/Lymphoid system",
+                "Immune system",
+                "Musculoskeletal system",
+                "Pulmonary/Respiratory system",
+                "Nervous system",
+                "Psychopathology system",
+                "Renal/Urinary system",
+                "Reproductive system",
+                "Skin/Connective system"
+            ],
+            "Step 1: Disciplines": [
+                "Step 1: Disciplines",
+                "Behavioral science",
+                "Biochemistry and Nutrition",
+                "Biostatistics",
+                "Cell biology",
+                "Embryology",
+                "Genetics",
+                "Gross anatomy",
+                "Histology",
+                "Immunology",
+                "Microbiology",
+                "Molecular biology",
+                "Neuroscience",
+                "Physiology",
+                "Pathology",
+                "Pathophysiology",
+                "Pharmacology/Therapeutics"
+            ],
+            "Step 1: Physician Task": [
+                "Applying foundational science concepts",
+                "Diagnosis",
+                "Management",
+                "Evidence-Based Medicine"
+            ]
+        },
+        keyTopicCatalogs: ["Subcategory 1 tested", "Subcategory 2 tested"]
+    },
+    1111444444442: {
+        name: "USMLEStep2",
+        taxonomies: {
+            "Normal Conditions and Disease Categories": [
+                "Immune",
+                "Blood & Lymphoreticular System",
+                "Behavioral Health",
+                "Nervous System and Special Senses",
+                "Musculoskeletal System",
+                "Skin/Subcutaneous Tissue",
+                "Cardiovascular System",
+                "Respiratory System",
+                "Gastrointestinal System",
+                "Renal & Urinary Systems & Male Reproductive",
+                "Pregnancy, Childbirth & the Puerperium",
+                "Female Reproductive & Breast",
+                "Endocrine System",
+                "Multisystem Processes & Disorders",
+                "General Principles of Foundational Science",
+                "Biostatistics & Epidemiology/Population Health",
+                "Interpretation of the Medical Literature"
+            ],
+            "Core Clinical Science": [
+                "Medicine",
+                "Neuroscience",
+                "Obstetrics & Gynecology",
+                "Pediatrics",
+                "Psychiatry",
+                "Surgery"
+            ],
+            "Step2: Physician Task": [
+                "Applying Foundational Science Concepts",
+                "Diagnosis",
+                "Health Maint, Prevention & Surveillance",
+                "Pharmacotherapy, Intervention, & Management"
+            ]
+        },
+        keyTopicCatalogs: ["Chief complaint", "Primary disease"]
+    },
+    1111777777771: {
+        name: "ANZBasicScience",
+        taxonomies: {
+            "UK Basic Sciences - Systems": [
+                "Cardiovascular system",
+                "Endocrine system",
+                "Gastrointestinal system",
+                "Haematological system",
+                "Immune system",
+                "Musculoskeletal system",
+                "Nervous system",
+                "Psychological system",
+                "Renal/Urinary system",
+                "Reproductive system",
+                "Respiratory system",
+                "Skin/Cancer tissue"
+            ],
+            "UK Basic Sciences - Disciplines": [
+                "Anatomy",
+                "Biochemistry",
+                "Cell biology",
+                "Embryology",
+                "Energy metabolism",
+                "Epidemiology",
+                "Genetics",
+                "Histology",
+                "Immunology",
+                "Microbiology",
+                "Molecular biology",
+                "Neurology",
+                "Nutrition",
+                "Pathology",
+                "Pathophysiology",
+                "Pharmacology/Therapeutics",
+                "Physiology",
+                "Public health"
+            ]
+        }
+    },
+    1111777777772: {
+        name: "ANZClinicalMedicine",
+        taxonomies: {
+            "UK Clinical Medicine - Competencies": [
+                "Communication Skills",
+                "Ethics",
+                "Interpretation of Results",
+                "Investigation",
+                "Making a Diagnosis/decision",
+                "Management",
+                "Mechanisms of Disease",
+                "Peri-Operative Care",
+                "Prevention",
+                "Prognosis",
+                "Symptoms and Signs"
+            ],
+            "UK Clinical Medicine - Specialties": [
+                "Anaesthetics",
+                "Dermatology",
+                "Emergency Medicine",
+                "Endocrinology",
+                "ENT",
+                "Gastroenterology",
+                "General Medicine",
+                "Haematology",
+                "Infectious Diseases",
+                "Molecular Biology/Genetics",
+                "Obstetrics and Gynaecology",
+                "Ophthalmology",
+                "Orthopaedics",
+                "Paediatrics",
+                "Pathology/Immunology",
+                "Psychiatry",
+                "Rheumatology",
+                "Surgery"
+            ],
+            "UK Clinical Medicine - Systems": [
+                "Cardiovascular system",
+                "Endocrine system",
+                "Gastrointestinal system",
+                "Haematological system",
+                "Immune system",
+                "Musculoskeletal system",
+                "Nervous system",
+                "Psychological system",
+                "Renal/Urinary system",
+                "Reproductive system",
+                "Respiratory system",
+                "Skin/Cancer tissue"
+            ]
+        }
+    },
+    1111888888881: {
+        name: "INDBasicScience",
+        taxonomies: {
+            "IND Basic Sciences - Systems": [
+                "Basic sciences application in clinical context",
+                "Cardiovascular system",
+                "Endocrine system",
+                "Energy metabolism",
+                "Gastrointestinal system",
+                "General",
+                "Haematological system",
+                "Immune system",
+                "Knowledge",
+                "Musculoskeletal system",
+                "Nervous system",
+                "Psychological system",
+                "Renal/Urinary system",
+                "Reproductive system",
+                "Respiratory system",
+                "Skin/Cancer Tissue"
+            ],
+            "IND Basic Sciences - Disciplines": [
+                "Anatomy",
+                "Biochemistry",
+                "Community Medicine",
+                "Forensic Medicine",
+                "Microbiology",
+                "Pathology",
+                "Pharmacology/Toxicology",
+                "Physiology"
+            ]
+        }
+    },
+    1111888888882: {
+        name: "INDClinicalMedicine",
+        taxonomies: {
+            "IND Clinical Medicine - Competencies": [
+                "Communication Skills",
+                "Ethics",
+                "Interpretation of Results",
+                "Investigation",
+                "Making a Diagnosis/decision",
+                "Management",
+                "Mechanisms of Disease",
+                "Peri-Operative Care",
+                "Prevention",
+                "Prognosis",
+                "Symptoms and Signs"
+            ],
+            "IND Clinical Medicine - Specialties": [
+                "Anesthesia",
+                "Dermatology",
+                "ENT",
+                "Medicine",
+                "Obstetrics and Gynaecology",
+                "Ophthalmology",
+                "Orthopaedics",
+                "Paediatrics",
+                "Psychiatry",
+                "Radiology",
+                "Surgery"
+            ],
+            "IND Clinical Medicine - Systems": [
+                "Cardiovascular system",
+                "Endocrine system",
+                "Gastrointestinal system",
+                "Haematological system",
+                "Immune system",
+                "Musculoskeletal system",
+                "Nervous system",
+                "Psychological system",
+                "Renal/Urinary system",
+                "Reproductive system",
+                "Respiratory system",
+                "Skin/Cancer tissue"
+            ]
+        }
+    }
+};
